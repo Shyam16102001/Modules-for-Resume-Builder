cc028f8e1ebab3785a3b1798d1691894d89f51a9
Shyam16102001 <55325014+Shyam16102001@users.noreply.github.com>
2023-05-29 20:46:43 +0530

    Bug Fix

--- generate/js/custom.js
+++
@@ -1,9 +0,0 @@
-const checkboxes = document.querySelectorAll(".employee-checkbox");
-checkboxes.forEach(function (checkbox) {
-  checkbox.addEventListener("change", function () {
-    const hikePercentageContainer = this.parentNode.querySelector(
-      ".hike-percentage-container"
-    );
-    hikePercentageContainer.style.display = this.checked ? "block" : "none";
-  });
-});
